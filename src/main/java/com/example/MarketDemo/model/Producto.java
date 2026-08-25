6032d666a3987a2d801cb01f927ba5fdd055524d
DevNico2905 <nbernal36@unisalle.edu.co>
2026-08-25 14:25:13 -0500

    Entities and rela created

--- src/main/java/com/example/MarketDemo/model/Producto.java
+++ src/main/java/com/example/MarketDemo/model/Producto.java
@@ -1,4 +1,25 @@
 package com.example.MarketDemo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "producto")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private Double precio;
+    private int cantidad;
 }
--- src/main/java/com/example/MarketDemo/model/Venta.java
+++ src/main/java/com/example/MarketDemo/model/Venta.java
@@ -1,4 +1,25 @@
 package com.example.MarketDemo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "venta")
 public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate fecha;
+    private String estado;
+    @ManyToOne
+    private Sucursal sucursal;
+
 }
