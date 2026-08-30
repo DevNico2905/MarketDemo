51854600feaf47959a7a1926fc6fe62a62be37be
DevNico2905 <nbernal36@unisalle.edu.co>
2026-08-29 21:02:38 -0500

    @Repository Annotation added

--- src/main/java/com/example/MarketDemo/repository/ProductoRepository.java
+++ src/main/java/com/example/MarketDemo/repository/ProductoRepository.java
@@ -1,7 +1,8 @@
 package com.example.MarketDemo.repository;
-
 import com.example.MarketDemo.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
--- src/main/java/com/example/MarketDemo/repository/VentaRepository.java
+++ src/main/java/com/example/MarketDemo/repository/VentaRepository.java
@@ -1,7 +1,8 @@
 package com.example.MarketDemo.repository;
-
 import com.example.MarketDemo.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
 }
