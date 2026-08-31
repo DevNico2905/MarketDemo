9d6624cdfa9e3975d4765c8c4932895a3434bec6
DevNico2905 <nbernal36@unisalle.edu.co>
2026-08-30 20:31:05 -0500

    @Builder Annotations added.

--- src/main/java/com/example/MarketDemo/model/Venta.java
+++ src/main/java/com/example/MarketDemo/model/Venta.java
@@ -1,10 +1,7 @@
 package com.example.MarketDemo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,6 +10,7 @@ import java.util.List;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "venta")
 public class Venta {
 
